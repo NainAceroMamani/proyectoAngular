cfb6c48da7392925422f2d82d6d394cc202b049a
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-05-29 11:59:30 -0500

    Promesas and Observables and Title and MetaDatos

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  private titulo: string;
+
+  constructor(
+    private router: Router,
+    private title: Title,
+    private meta: Meta ) {
+
+    this.getDataRoute().subscribe( data => {
+      // console.log(data);
+      this.titulo = data.titulo;
+      this.title.setTitle( this.titulo );
+
+      this.meta.addTags([
+        { name: 'name', content: this.titulo },
+        { property: 'og:title', content: 'Feria Tacna' },
+        { property: 'og:type', content: 'website' },
+        { charset: 'UTF-8' }
+      ]);
+
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter( evento => evento instanceof ActivationEnd ),
+      filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null ),
+      map( (evento: ActivationEnd) => evento.snapshot.data )
+    );
   }
 
 }
